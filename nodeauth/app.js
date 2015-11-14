a3faa9c83d48f8c02418a63e7c85ededd130ec74
suzp1984 <suzp1984@gmail.com>
2015-11-14 21:18:07 +0800

    add bootstrap modle and login, register views

--- nodeauth/app.js
+++ nodeauth/app.js
@@ -5,13 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var expressValidator = require('express-validator');
 var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var multer = require('multer');
 var flash = require('connect-flash');
 var mongo = require('mongodb');
-var mongooes = require('mongooes');
-var db = mongooes.connection;
+var mongoose = require('mongoose');
+var db = mongoose.connection;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -22,13 +23,52 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//handle file uploads
+app.use(multer({dest: './uploads'}).single('upload'));
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//handle Express session
+app.use(session({
+    secret:'secret',
+    saveUninitialized: true,
+    resave:true
+}));
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(flash());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);

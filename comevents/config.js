36dfab9d4d0a2fe0395fd25df4563acbf0a6ba3f
suzp1984 <suzp1984@gmail.com>
2015-11-18 22:43:01 +0800

    config update

--- comevents/config.js
+++ comevents/config.js
@@ -5,8 +5,8 @@ exports.mongodb = {
   uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/comevents'
 };
 exports.companyName = 'Zpcat Home';
-exports.projectName = 'Zpcat';
-exports.systemEmail = 'zpcat@email.addy';
+exports.projectName = 'Communite Events';
+exports.systemEmail = 'suzh1984@163.com';
 exports.cryptoKey = 'k3yb0ardc4t';
 exports.loginAttempts = {
   forIp: 50,
@@ -17,11 +17,11 @@ exports.requireAccountVerification = false;
 exports.smtp = {
   from: {
     name: process.env.SMTP_FROM_NAME || exports.projectName +' Website',
-    address: process.env.SMTP_FROM_ADDRESS || 'your@email.addy'
+    address: process.env.SMTP_FROM_ADDRESS || 'suzh1984@163.com'
   },
   credentials: {
-    user: process.env.SMTP_USERNAME || 'zpcat@email.addy',
-    password: process.env.SMTP_PASSWORD || 'bl4rg!',
+    user: process.env.SMTP_USERNAME || 'suzh1984@163.com',
+    password: process.env.SMTP_PASSWORD || 'xxxxxxxxx',
     host: process.env.SMTP_HOST || 'smtp.163.com',
     ssl: true
   }
